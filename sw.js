6ad4dc2af246b54da9c8b3f50f2921b67ad88abd
miguelnsouza1 <miguelnsouza1@gmail.com>
2026-08-19 14:43:50 -0300

    Atualizar cache para eleições municipais

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'voto-guia-shell-v4';
+const CACHE_VERSION = 'voto-guia-shell-v5';
 const BASE_URL = new URL('./', self.registration.scope).href;
 const APP_SHELL = [
   BASE_URL,
